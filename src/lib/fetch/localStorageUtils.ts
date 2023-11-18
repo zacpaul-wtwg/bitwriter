ab12457b3ba68121bc21e630518e13c27c4a13bb
Zac Paul <zac@websitesthatworkgood.com>
2023-11-18 18:14:03 -0500

    bug fixes

--- src/lib/fetch/localStorageUtils.ts
+++ src/lib/fetch/localStorageUtils.ts
@@ -1,12 +1,22 @@
 // /src/lib/localStorageUtils.ts
-"use client"
+
 // Function to get data from local storage
 export const getLocalStorage = (key: string) => {
+    // Check if running in a browser environment
+    if (typeof window === 'undefined') {
+        return null;
+    }
+
     const storedValue = localStorage.getItem(key);
     return storedValue ? JSON.parse(storedValue) : null;
 };
 
 // Function to set data in local storage
 export const setLocalStorage = (key: string, value: any) => {
+    // Check if running in a browser environment
+    if (typeof window === 'undefined') {
+        return;
+    }
+
     localStorage.setItem(key, JSON.stringify(value));
 };
